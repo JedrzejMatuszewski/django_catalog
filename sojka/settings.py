0850bb99397b31916f26ce890c985bdfd845fb36
Jędrzej Matuszewski <matuszewski99@gmail.com>
2020-08-11 12:10:29 +0200

    Updated Settings

--- sojka/settings.py
+++ sojka/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r*6!4l)oi_sve(mvaj2uy$e_d6m)43jil(4b%-+gnhsx2g5x!-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
