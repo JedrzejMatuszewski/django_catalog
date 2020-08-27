86c629458a9c2efc30c302812c2e461259b59613
Jędrzej Matuszewski <matuszewski99@gmail.com>
2020-08-27 16:55:15 +0200

    Updated page

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.template.defaultfilters import slugify
+import math
 
 class Category(models.Model):
     title = models.CharField(max_length=50)
@@ -24,12 +25,17 @@ class Product(models.Model):
     sale = models.IntegerField(default=0)
     auction_url = models.CharField(max_length=200, default="#")
     img = models.ImageField(default='img/default-product-img.jpg', upload_to='img/')
-
     slug = models.SlugField(null=True, unique=True)
 
     def __str__(self):
         return self.title
 
+    def __gt__(self, other):
+        return self.title > other.title
+
+    def show_promo(self):
+        return round(100 - (self.sale*100)/self.price)
+    
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
@@ -37,6 +43,7 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product-detail', kwargs={'slug': self.slug})
+
 
 class ProductGallery(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
